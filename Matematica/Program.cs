967ff81c586b2229e9eeef7b38b552b0c24cce46
Nicola Galeotti <nicola.galeotti9@gmail.com>
2019-10-04 18:15:59 +0100

    fine primo programma; fine metodo

--- Matematica/Program.cs
+++ Matematica/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OperazioniMatematica;
 
 namespace Matematica
 {
@@ -6,7 +7,51 @@ namespace Matematica
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Inserisci il primo numero: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il secondo numero: ");
+            int n2 = int.Parse(Console.ReadLine());
+            try
+            {
+                double risultato;
+                Console.WriteLine("Quale operazione si vuole fare? (Add/Sot/Mol/Div/Pot/Min/Max)");
+                string risposta = Console.ReadLine();
+                if (risposta == "Add")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Somma(n1, n2);
+                }
+                else if (risposta == "Sot")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Differenza(n1, n2);
+                }
+                else if (risposta == "Mol")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Moltiplicazione(n1, n2);
+                }
+                else if (risposta == "Div")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Divisione(n1, n2);
+                }
+                else if (risposta == "Pot")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Potenza(n1, n2);
+                }
+                else if (risposta == "Min")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Minimo(n1, n2);
+                }
+                else if (risposta == "Max")
+                {
+                    risultato = OperazioniMatematica.OperazioniMatematica.Massimo(n1, n2);
+                }
+                else risultato = double.NaN;
+                Console.WriteLine(risultato);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ OperazioniMatematica/Operazioni.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace OperazioniMatematica
+{
+    public class OperazioniMatematica
+    {
+        public static int Somma(int a, int b)
+        {
+            int s = a + b;
+            return s;
+        }
+        public static int Differenza(int a, int b)
+        {
+            int d = a - b;
+            return d;
+        }
+        public static int Moltiplicazione(int a, int b)
+        {
+            int p = a * b;
+            return p;
+        }
+        public static double Divisione(double a, double b)
+        {
+            if (b <= 0)
+            {
+                throw new Exception("Il divisore non può essere uguale o minore di 0!");
+            }
+            else
+            {
+                double q = a / b;
+                return q;
+            }
+        }
+        public static long Potenza(int a, int b)
+        {
+            int c = 0;
+            long p = 1;
+            while (c < b)
+            {
+                p = p * a;
+                c++;
+            }
+            return p;
+        }
+        public static int Minimo(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        public static int Massimo(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
